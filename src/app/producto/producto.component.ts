fda8aab640518f13d4dca8c1ea90cebdaee2d24e
Bosnan_Lab <121900154+bosnan-lab@users.noreply.github.com>
2025-06-13 11:04:10 -0600

    propiedades y plantillas

--- src/app/producto/producto.component.ts
+++ src/app/producto/producto.component.ts
@@ -11,11 +11,13 @@ export class ProductoComponent {
   public nombre: string;
   public marca: string;
   public precio: number;
+  public descripcion: string;
 
   constructor(){
     this.nombre = "PC Portátil";
     this.marca = "Asus";
     this.precio = 1200;
+    this.descripcion = "Este portatil es muy potente";
 
     console.log("ProductoComponent initialized");
   }
